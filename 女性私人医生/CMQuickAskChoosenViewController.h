8bac334371db0f976944c787d7c378f5bdefa262
zxt3310 <xintaozhang88@gmail.com>
2017-04-10 18:48:52 +0800

    4.10

---
+++ 女性私人医生/CMQuickAskChoosenViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMQuickAskChoosenViewController.h
+//  女性私人医生
+//
+//  Created by 张信涛 on 2017/4/10.
+//  Copyright © 2017年 Tim. All rights reserved.
+//
+
+#import "CustomBaseViewController.h"
+
+@interface CMQuickAskChoosenViewController : CustomBaseViewController
+
+@end
---
+++ 女性私人医生/CMQuickAskChoosenViewController.m
@@ -0,0 +1,37 @@
+//
+//  CMQuickAskChoosenViewController.m
+//  女性私人医生
+//
+//  Created by 张信涛 on 2017/4/10.
+//  Copyright © 2017年 Tim. All rights reserved.
+//
+
+#import "CMQuickAskChoosenViewController.h"
+
+@interface CMQuickAskChoosenViewController ()
+
+@end
+
+@implementation CMQuickAskChoosenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
